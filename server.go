61fb1dc86ff56ff5633983fcfd7a1d24d53c3e41
Peter Elliott <pelliott@ualberta.ca>
2019-05-26 19:58:10 -0700

    added server docs

--- server.go
+++ server.go
@@ -7,6 +7,10 @@ import (
 	"sync"
 )
 
+// Returns true if response is a valid Reverse HTTP upgrade Request
+// (i.e. a valid HTTP/1.1 protocol upgrade request where the Upgrade Header
+// is "PTTH/1.0).
+// This function will return False otherwise, including when req is nil.
 func IsReverseHTTPRequest(req *http.Request) bool {
 	if req == nil {
 		return false
@@ -41,6 +45,10 @@ func (it *ioTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	return resp, err
 }
 
+// Produces an http.Client from an http.ResponseWriter and http.Request.
+// This Client can be used for a single request. It is possible that the
+// connection is kept alive, and the client can be used more than once but this
+// behavior shouldn't be relied on.
 func ReverseRequest(w http.ResponseWriter, r *http.Request) (*http.Client, error) {
 	if !IsReverseHTTPRequest(r) {
 		return nil, errors.New("request is not a valid reverse http request")

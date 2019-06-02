e0f2f7262989b3206627dadd2207be7e5a5dfe3a
Peter Elliott <pelliott@ualberta.ca>
2019-06-02 15:12:12 -0700

    added reverse response benchmark

---
+++ benchmark_test.go
@@ -0,0 +1,70 @@
+package reversehttp
+
+import (
+	"testing"
+	"net/http"
+	"io"
+	"io/ioutil"
+	"bytes"
+)
+
+type benchmarkBody struct {
+	b      *testing.B
+	data   []byte
+	reader io.Reader
+}
+
+func newBenchmarkBody(data []byte, b *testing.B) *benchmarkBody {
+	return &benchmarkBody{b, data, bytes.NewReader(data)}
+}
+
+func newBenchmarkBodyReq(request *http.Request, b *testing.B) *benchmarkBody {
+	buf := new(bytes.Buffer)
+	request.Write(buf)
+	data, _ := ioutil.ReadAll(buf)
+	return newBenchmarkBody(data,b)
+}
+
+func (bb *benchmarkBody) reset() {
+	bb.reader = bytes.NewReader(bb.data)
+}
+
+func (bb *benchmarkBody) Close() error {
+	return nil
+}
+
+func (bb *benchmarkBody) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
+func (bb *benchmarkBody) Read(b []byte) (int, error) {
+	return bb.reader.Read(b)
+}
+
+func BenchmarkReverseResponse(b *testing.B) {
+	b.ReportAllocs()
+
+	var handler http.HandlerFunc
+	handler = func(w http.ResponseWriter, r *http.Request) {}
+
+	req, _ := http.NewRequest("GET", "http://example.com/path",
+		ioutil.NopCloser(bytes.NewReader([]byte("hello world\n"))))
+
+	h := http.Header{}
+	h.Add("upgrade", "PTTH/1.0")
+	h.Add("CONNECTION", "Upgrade")
+	r := &http.Response{
+		StatusCode: http.StatusSwitchingProtocols,
+		Header:     h,
+		Body:		newBenchmarkBodyReq(req, b),
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ReverseResponse(r, handler)
+		b.StopTimer()
+		r.Body.(*benchmarkBody).reset()
+		b.StartTimer()
+	}
+}
